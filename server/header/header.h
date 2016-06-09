1d15ef344b7efb397c8ff6cd5d481c6ae3646ba9
Itadakimas <said.ahemt@gmail.com>
2016-06-09 03:16:45 +0200

    adds missing headers for MacOS

--- server/header/header.h
+++ server/header/header.h
@@ -1,35 +1,35 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
-
 #include <iostream>
 #include <string>
 #include "cpu.h"
 #include "hdd.h"
 #include "memory.h"
 #include "os.h"
-
 #if defined(_WIN32)
-    #include <windows.h>
-	#include "cpuwin.hpp"
-    #include "memorywin.hpp"
-	#include "windowsos.hpp"
+  #include <windows.h>
+  #include "cpuwin.hpp"
+  #include "memorywin.hpp"
+  #include "windowsos.hpp"
 #elif defined(__APPLE__)
-	#include "cpumac.hpp"
-	#include "macos.hpp"
-	#include "memorymac.hpp"
+  #include <sys/mount.h>
+  #include <sys/param.h>
+  #include <sys/sysctl.h>
+  #include <sys/types.h>
+  #include <sys/utsname.h>
+  #include <unistd.h>
+  #include "cpumac.hpp"
+  #include "macos.hpp"
+  #include "memorymac.hpp"
 #else
-	#include "cpulinux.hpp"
-	#include "linuxos.hpp"
-	#include "memorylinux.hpp"
+  #include <sys/mount.h>
+  #include <sys/param.h>
+  #include <sys/sysctl.h>
+  #include <sys/types.h>
+  #include <sys/utsname.h>
+  #include <unistd.h>
+  #include "cpulinux.hpp"
+  #include "linuxos.hpp"
+  #include "memorylinux.hpp"
 #endif
-
-#ifdef __unix__
-	#include <sys/utsname.h>
-    #include <sys/types.h>
-    #include <sys/sysctl.h>
-    #include <sys/param.h>
-    #include <sys/mount.h>
-	#include <unistd.h>
-#endif
-
 #endif
